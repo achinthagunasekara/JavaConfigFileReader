13efd29ce99b726cdfd7471c3af2051cf2d13f17
Archie Gunasekara <achintha@me.com>
2015-09-07 12:16:27 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+#Java Config Reader
+
+Configuration file name is configured with the following variable
+
+```java
+private final String file = "config.properties";
+```
+
+ConfigFileReader reads all items on this file and load them into "properties" object.
+
+##Adding Config Items
+
+Open config.properties files in a test editor
+
+Add configuration items as below,
+
+```html
+PROPERTY1=PROPTERY1_VALUE
+PROPERTY2=PROPTERY2_VALUE
+PROPERTY3=PROPTERY3_VALUE
+PROPERTY4=PROPTERY4_VALUE
+.
+.
+.
+```
+
+Please note, because this library loads all configuration items when it first gets a config item, any properties that are added after that even won't be available until the application is restarted. 
+
+##Accessing Config Items
+
+This configuration items can be accesses as below,
+
+```java
+ConfigFileReader config = ConfigFileReader.getConfigFileReaderInstance();
+config.getPropertyFor("CONFIG_ITEM");
+```
+
+This returns a String object.
---
+++ src/Config/ConfigFileReader.java
@@ -0,0 +1,43 @@
+package Config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ *
+ * @author Achintha Gunasekara
+ * @date 07.09.2015
+ */
+
+public class ConfigFileReader {
+    
+    //Configuration file used by the config reader
+    private final String file = "config.properties";
+    private final Properties properties;
+    private static ConfigFileReader instance;
+    
+    public static ConfigFileReader getConfigFileReaderInstance() throws IOException {
+	
+        //there can only be once instance of the config reader
+        if(instance == null) {
+			
+            instance = new ConfigFileReader();
+        }
+        
+        return instance;
+    }
+    
+    //read and load the config file
+    private ConfigFileReader() throws IOException {
+		
+        properties = new Properties();
+        properties.load(new FileInputStream(file));
+    }
+    
+    //get the propery value for strng s
+    public String getPropertyFor(String s) {
+        
+        return properties.getProperty(s);
+    }
+}
